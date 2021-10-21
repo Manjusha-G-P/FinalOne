9e8f9095cfdef438ea693cc660c13bdd04000f81
Manjusha-G-P <manjushagp96@gmail.com>
2021-10-21 10:25:54 +0530

    My First Commit

---
+++ spec.js
@@ -0,0 +1,8 @@
+describe('Protractor Demo', () => {
+	it('Should navigate to Demo Page', () => {
+		browser.get('http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx');
+		browser.getCurrentUrl().then((url) => {
+			expect(url).toBe('http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx');
+		});
+	});
+});
